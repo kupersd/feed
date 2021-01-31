ac409ac347722ae54f8f8bf3d0c98f1eddba1789
kupersd <kupersd@gmail.com>
2021-01-31 10:36:26 +0200

    beta

--- final-e2e-proj/frontend/src/App.jsx
+++ final-e2e-proj/frontend/src/App.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 
 import { Home } from './pages/Home'
-import { LoginSignup } from './pages/LoginSignup'
-import { Chat } from './pages/Chat'
-import { UserDetails } from './pages/UserDetails'
+import { Feed } from './pages/backendFeed'
 
 import { Header } from './cmps/Header'
 
@@ -15,10 +13,8 @@ export function App() {
       <Router>
         <Header></Header>
         <main>
-          <Switch>
-            <Route path="/user/:id" component={UserDetails} />
-            <Route path="/login" component={LoginSignup} />
-            <Route path="/chat" component={Chat} />
+          <Switch>            
+            <Route path="/feed" component={Feed} />
             <Route path="/" component={Home} />
           </Switch>
         </main>
---
+++ final-e2e-proj/frontend/src/store/actions/msgActions.js
@@ -0,0 +1,35 @@
+import { msgService } from '../../services/msgService'
+
+export function loadMsgs() {
+  return async dispatch => {
+    try {
+      const msgs = await msgService.query()
+      dispatch({ type: 'SET_MSGS', msgs: msgs })
+
+    } catch (err) {
+      console.log('MsgActions: err in loadMsgs', err)
+    }
+  }
+}
+
+export function addMsg(msg) {
+  return async dispatch => {
+    try {
+      const addedMsg = await msgService.add(msg)
+      dispatch({ type: 'ADD_MSG', msg: addedMsg })
+    } catch (err) {
+      console.log('MsgActions: err in addMsg', err)
+    }
+  }
+}
+
+export function removeMsg(msgId) {
+  return async dispatch => {
+    try {
+      await msgService.remove(msgId)
+      dispatch({ type: 'REMOVE_MSG', msgId })
+    } catch (err) {
+      console.log('MsgActions: err in removeMsg', err)
+    }
+  }
+}
