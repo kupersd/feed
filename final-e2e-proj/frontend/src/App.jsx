47959499579f21fc7567aaeac06e6370699835d7
Ori Weinstock <ori.weinstock@gmail.com>
2021-01-31 10:37:37 +0200

    form done

--- final-e2e-proj/frontend/src/App.jsx
+++ final-e2e-proj/frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { Home } from './pages/Home'
 import { LoginSignup } from './pages/LoginSignup'
 import { Chat } from './pages/Chat'
 import { UserDetails } from './pages/UserDetails'
+import { Feed } from './pages/Feed'
 
 import { Header } from './cmps/Header'
 
@@ -13,17 +14,18 @@ export function App() {
   return (
     <div className="app">
       <Router>
-        <Header></Header>
+        {/* <Header></Header> */}
         <main>
           <Switch>
-            <Route path="/user/:id" component={UserDetails} />
-            <Route path="/login" component={LoginSignup} />
+            {/* <Route path="/user/:id" component={UserDetails} /> */}
+            {/* <Route path="/login" component={LoginSignup} /> */}
             <Route path="/chat" component={Chat} />
-            <Route path="/" component={Home} />
+            {/* <Route path="/feed" component={Feed} /> */}
+            <Route path="/" component={Feed} />
           </Switch>
         </main>
         <footer>
-          Starter
+          {/* Starter */}
         </footer>
       </Router>
     </div>
